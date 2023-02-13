4e12d34a8a1186e6f00fe8ccb3f010eda4a23802
Erik Jimenez Velasco <jive931221@gmail.com>
2023-02-12 21:17:07 -0600

    Mi primer cambio

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import HelloCard from '../components/hellocard/HelloCard';
 function HomePage() {
     return (
         <div>
-            <h1>Bienvenido a mi sitio personal</h1>
+            <h1>Bienvenido a mi sitio personal.</h1>
 
             <HelloCard nombre="Alan Jimenez" />
         </div>
