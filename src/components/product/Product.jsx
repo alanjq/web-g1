d30ff5f06955e81eb44b7b6089164cdd4961ee4a
Alan Jimenez <alanjq@outlook.com>
2023-02-15 23:06:09 -0500

    Update styles for Product component

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import './ProductStyles.scss'
+import ProductRating from './ProductRating'
 
 function Product({ category, description, id, image, price, rating, title }) {
+    // TODO: reusabilidad de componente
+    const handleAddItem = () => {
+        let itemToAdd = { category, description, id, image, price, rating, title }
+        console.log('Agregar el siguiente objeto al carrito', itemToAdd);
+    }
+
     return (
         <article className='product-item'>
             <h1 className='title'>{title}</h1>
             <span className='category'>{category}</span>
             <span className='id'>{id}</span>
-            <img className='image' src={image} width="100%" />
+            <img className='image' alt="" src={image} width="100%" />
             <span className='description'>{description}</span>
             <span className='price'>{price}</span>
-            {/* TODO: Create component */}
-            {/* Params: rate, count */}
-            {/* Eg. <Rating rate={rate} count={count} /> */}
+            <ProductRating rate={rating.rate} count={rating.count} />
+            <button type='button' onClick={handleAddItem}>Agregar al carrito</button>
         </article>
     )
 }
